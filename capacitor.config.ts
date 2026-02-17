fafd60b99c309999526c2db046255d74fc5804d7
Ing. Alan Christian Ramírez Rodriguez <ingenieroacramirez@gmail.com>
2026-02-17 10:01:00 -0600

    Agregar configuración del servidor en capacitor.config.ts
    - Incluir esquema de Android como 'https' y establecer el hostname para la aplicación.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,10 @@ const config: CapacitorConfig = {
   appId: 'com.residencialpass.app',
   appName: 'Residencial Pass',
   webDir: 'www',
+  server: {
+    androidScheme: 'https',
+    hostname: 'app.residencialpass.com',
+  },
 };
 
 export default config;
